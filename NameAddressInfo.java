f8c5271af1f447267ee304b39feb3aaff428c847
B-theartist <123841356+B-theartist@users.noreply.github.com>
2024-03-23 15:44:18 -0600

    Add files via upload

---
+++ NameAddressInfo.java
@@ -0,0 +1,28 @@
+
+
+public class NameAddressInfo {
+    public static void main(String[] args) {
+       // SET firstName TO "Barry"
+       // SET lastName TO "Allen"
+       // SET streetAddress TO "1956 Showcase Drive Apt #4"
+       // SET city TO "Central City"
+       // SET zipCode TO "12345"
+        String firstName = "Barry";
+        String lastName = "Allen";
+        String streetAddress = "1956 Showcase Drive Apt #4";
+        String city = "Central City";
+        String zipCode = "12345";
+        
+      //  PRINT firstName
+      // PRINT lastName
+       // PRINT streetAddress
+      // PRINT city
+      //  PRINT zipCode
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+
+}
